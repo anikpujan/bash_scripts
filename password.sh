0bcfab61766116a875ec704db621dc55327325da
anikpujan <anikpujan@gmail.com>
2021-03-31 18:52:34 +0000

    Assignments

--- password.sh
+++ password.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-# read TITLE
-# regex="$(grep -E '.{8}' <<< "$1" | grep '[0-9]' | grep '[@#$]')"
-# if [[ $regex=~$TITLE ]]; then
-#   echo "INVALID"
-# else
-#   echo "Valid"
-# fi
-
-
-echo "Please enter password for User to be created in OIM: "
-echo "******Please Note: Password should be at least 6 characters long with one digit and one Upper case Alphabet******"
-read user_passwd
-regex="${#s} -ge 6 && "$s" == *[[:lower:]]* && "$s" == *[[:upper:]]* && "$s" == *[0-9]*"
-echo $user_passwd
-echo $regex
-if [[ $user_passwd =~ $regex ]]; then
-    echolog "Password Matches the criteria"
+echo "-------------------------------"
+echo "Password Validation"
+echo "--------------------------------"
+echo "Enter your Password"
+read -s password
+len="${#password}"
+if [[ $len -lt 8 ]];
+ then
+    echo "Password must be atleast 8 character!!!"
+     
+elif echo  $password | grep "[a-z]" | grep "[A-Z]" | grep "[0-9]"
+then
+    echo "Password strong"
 else
-    echo "Password criteria: Password should be at least 8 characters long with one digit and one Upper case Alphabet"
-    echo "Password does not Match the criteria, exiting..."
-    exit
+    echo "Strong password needed"
 fi
+
